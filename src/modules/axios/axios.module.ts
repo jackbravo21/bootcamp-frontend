ff0df85f1c786dc5b5ee2a50352790eeafcbb7ed
jackbravo21 <kaldnar@hotmail.com>
2022-01-18 00:49:25 -0300

    Rota Ajustada

--- src/modules/axios/axios.module.ts
+++ src/modules/axios/axios.module.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8082",
+    baseURL: "http://localhost:5000/dev/",
 });
 
 export default axiosInstance;
